bbe8192c9d8f3abfe5cea39c146a8597e7e7f6ef
zrajani <zainrajani2207@gmail.com>
2020-06-17 21:30:56 -0400

    Updates the Output as per the rules

--- includes/gol_config.h
+++ includes/gol_config.h
@@ -13,21 +13,21 @@
 /*
  * "community of cells" (handled by one thread) parameters
  */
-#define config_NC		32 // # of cell rows in a community
+#define config_NC		16 // # of cell rows in a community
 #define config_MC		16 // # of cell columns in a community
 
 /*
  * overall environment parameters
  */
-#define config_K		1 // # of communities "down"
-#define config_L		4 // # of communities "across"
+#define config_K		2 // # of communities "down"
+#define config_L		8 // # of communities "across"
 #define config_NE 		config_K*config_NC // number of environment rows
 #define config_ME 		config_L*config_MC // number of environment columns
 
 /*
  * temporal parameters
  */
-#define config_TL		1048576 // microseconds between generation
+#define config_TL		262144.5 // microseconds between generation
 #define	config_TDISP	1 // number of generations between plots
 
 /*
